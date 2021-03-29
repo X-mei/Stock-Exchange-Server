394d3247140c53793155c5c4f49b1923a57feebc
mg476 <mg476@duke.edu>
2021-03-28 23:33:59 -0400

    add lock

--- server/parser.h
+++ server/parser.h
@@ -2,7 +2,7 @@
 #define PARSER_H
 #include "common.h"
 #include "database.h"
-
+static mutex mtx;
 string do_create(pugi::xml_document& doc, Database* db);
 string do_transactions(pugi::xml_document& doc, Database* db);
 #endif
